6aa5107f2c8f9dae1f092e80104a6e7adc22d404
amichalo421 <andre.michalowski@gmail.com>
2020-11-16 18:52:37 -0500

    Restricted middleware functioning

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,23 +1,21 @@
 const jwt = require("jsonwebtoken");
 
-const { jwtSecret } = require("../api/config.js");
+const { jwtSecret } = require("../api/secret.js");
 
 module.exports = (req, res, next) => {
     const token = req.headers.authorization;
 
     if (token) {
         jwt.verify(token, jwtSecret, (err, decodedToken) => {
-            if (err) {
-                // token is invalid
+            if (err) { // token is invalid
                 res.status(401).json({ you: "Cant't touch this!" });
-            } else {
-                // token is valid
+                console.log(err);
+            } else { // token is valid
                 req.jwt = decodedToken;
-
                 next();
             }
         });
     } else {
-        res.status(401).json({ you: "shall not pass!" });
+        res.status(401).json({ none: "shall not pass! We are the knights of ni!" });
     }
 };
--- routes/users/users-router.js
+++ routes/users/users-router.js
@@ -1,16 +1,27 @@
 const router = require("express").Router();
 
 const Users = require("./users-modal.JS");
+const restricted = require("../../auth/restricted-middleware.js");
 
-router.get("/test", (req, res) => { 
+router.get("/test", restricted, /*checkRole()*/ (req, res) => { 
     Users.findOperators()
-        .then(users => {
-            res.status(200).json({ users });
+        .then(operators => {
+            res.status(200).json({ operators, jwt: req.jwt });
         })
         .catch(err=> {
           res.status(404).json({ message: "test endpoint error"}).send(err);
           console.log(err);
         });
 });
+
+// function checkRole(role) { // 1. write it
+//   return function (req, res, next) {
+//       if (req.jwt.role === role) {
+//           next();
+//       } else {
+//           res.status(403).json({ message: "you have no access" });
+//       }
+//   };
+// }
 
 module.exports = router;
